d0af9ee65ecc3edcbf3ab6b005e7b382e3d1a576
Nikhil Dhole <nikhildadadhole@gmail.com>
2025-09-05 00:22:28 +0530

    fix for test of code

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 import prettierPlugin from 'eslint-plugin-prettier';
 import prettierConfig from 'eslint-config-prettier';
+import jestPlugin from 'eslint-plugin-jest';
 
 export default [
   {
@@ -24,6 +25,20 @@ export default [
       ...pluginJs.configs.recommended.rules,
       ...prettierConfig.rules,
       'prettier/prettier': 'error',
+    },
+  },
+  {
+    files: ['**/__tests__/**/*.js', '**/*.test.js'],
+    plugins: {
+      jest: jestPlugin,
+    },
+    languageOptions: {
+      globals: {
+        ...globals.jest, // ✅ add Jest globals like test/expect/describe
+      },
+    },
+    rules: {
+      ...jestPlugin.configs.recommended.rules,
     },
   },
 ];
